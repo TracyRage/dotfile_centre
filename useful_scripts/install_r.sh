05c2a242de7c0b9ed7aa0e7293051e88dfdb9e31
TracyRage <alexei.remizovschi@protonmail.ch>
2022-01-27 16:08:28 +0200

    Changed: inatall_r

--- useful_scripts/install_r.sh
+++ useful_scripts/install_r.sh
@@ -6,6 +6,9 @@ set -u
 set -o pipefail
 
 
+OSNAME=ubuntu
+OSVERS=focal
+
 # Check for existing R and packages
 dpkg -l | grep -E "(r-base-core|cran)"
 
@@ -16,8 +19,6 @@ sudo apt-get purge -y r-base-core
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9
 
 # Add CRAN repo
-OSNAME=$(. /etc/os-release; echo "$ID")
-OSVERS=$(lsb_release -cs)
 sudo add-apt-repository "deb http://cran.rstudio.com/bin/linux/$OSNAME $OSVERS-cran40/"
 
 # Update the package list
@@ -28,21 +29,21 @@ sudo apt-get -y install r-base
 
 # Install other staff
 sudo apt-get -y install \
-	  htop \
-		  libcurl4-openssl-dev \
-			  libssl-dev \
-				  libgeos-dev \
-					  libgdal-dev \
-						  libproj-dev \
-							  libxml2-dev \
-								  libudunits2-dev \
-									  libsodium-dev \
-										  pandoc \
-											  openjdk-8-jdk \
-												  openjdk-8-jre \
-													  cargo \
-														  libcairo2-dev \
-															  libfreetype6-dev \
-																  libclang-dev \
-																	  ttf-mscorefonts-installer \
-																		  fonts-roboto
+htop \
+libcurl4-openssl-dev \
+libssl-dev \
+libgeos-dev \
+libgdal-dev \
+libproj-dev \
+libxml2-dev \
+libudunits2-dev \
+libsodium-dev \
+pandoc \
+openjdk-8-jdk \
+openjdk-8-jre \
+cargo \
+libcairo2-dev \
+libfreetype6-dev \
+libclang-dev \
+ttf-mscorefonts-installer \
+fonts-roboto
